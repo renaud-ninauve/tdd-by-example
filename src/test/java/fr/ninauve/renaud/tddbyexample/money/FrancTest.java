37daad10ff4655b8ba48ddec972eaa1cec4642bc
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-06-17 16:13:31 +0200

    [green] add missing Franc equality test

--- src/test/java/fr/ninauve/renaud/tddbyexample/money/FrancTest.java
+++ src/test/java/fr/ninauve/renaud/tddbyexample/money/FrancTest.java
@@ -12,4 +12,10 @@ class FrancTest {
     assertThat(five.times(2)).isEqualTo(new Franc(10));
     assertThat(five.times(3)).isEqualTo(new Franc(15));
   }
+
+  @Test
+  void equality() {
+    assertThat(new Franc(5)).isEqualTo(new Franc(5));
+    assertThat(new Franc(5)).isNotEqualTo(new Franc(6));
+  }
 }
