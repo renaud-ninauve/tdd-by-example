20918db1989bad90a2cd4c99a8d286016dfea691
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-06-17 11:29:22 +0200

    [red] side effects

--- src/test/java/fr/ninauve/renaud/tddbyexample/money/MultiplicationTest.java
+++ src/test/java/fr/ninauve/renaud/tddbyexample/money/MultiplicationTest.java
@@ -10,9 +10,9 @@ class MultiplicationTest {
   void testMultiplication() {
     Dollar five = new Dollar(5);
     Dollar product = five.times(2);
-    assertThat(five.amount).isEqualTo(10);
+    assertThat(product.amount).isEqualTo(10);
 
     product = five.times(3);
-    assertThat(five.amount).isEqualTo(15);
+    assertThat(product.amount).isEqualTo(15);
   }
 }
