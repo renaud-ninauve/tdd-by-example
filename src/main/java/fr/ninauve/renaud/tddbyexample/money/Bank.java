e1dc258980f959e21a380683fc7ca4c87e5dc9b6
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-06-18 09:26:52 +0200

    [green] reduce Money

--- src/main/java/fr/ninauve/renaud/tddbyexample/money/Bank.java
+++ src/main/java/fr/ninauve/renaud/tddbyexample/money/Bank.java
@@ -2,8 +2,11 @@ package fr.ninauve.renaud.tddbyexample.money;
 
 public class Bank {
 
-  public Money reduce(Expression expression, String to) {
-    final Sum sum = (Sum) expression;
+  public Money reduce(Expression source, String to) {
+    if (source instanceof Money) {
+      return (Money) source;
+    }
+    final Sum sum = (Sum) source;
     return sum.reduce(to);
   }
 }
