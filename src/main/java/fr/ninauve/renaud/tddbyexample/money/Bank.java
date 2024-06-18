bbb965e22c703d40e2a2a83e4f740bff233ee219
renaud-ninauve <renaud.ninauve.92@gmail.com>
2024-06-18 09:43:04 +0200

    [green] ask bank for rate

--- src/main/java/fr/ninauve/renaud/tddbyexample/money/Bank.java
+++ src/main/java/fr/ninauve/renaud/tddbyexample/money/Bank.java
@@ -3,9 +3,13 @@ package fr.ninauve.renaud.tddbyexample.money;
 public class Bank {
 
   public Money reduce(Expression source, String to) {
-    return source.reduce(to);
+    return source.reduce(this, to);
   }
 
   public void addRate(String from, String to, int rate) {
+  }
+
+  public int rate(String from, String to) {
+    return from.equals("CHF") && to.equals("USD") ? 2 : 1;
   }
 }
--- src/main/java/fr/ninauve/renaud/tddbyexample/money/Sum.java
+++ src/main/java/fr/ninauve/renaud/tddbyexample/money/Sum.java
@@ -10,7 +10,7 @@ public class Sum implements Expression {
   }
 
   @Override
-  public Money reduce(String to) {
+  public Money reduce(Bank bank, String to) {
     return new Money(augend.amount + addend.amount, to);
   }
 }
